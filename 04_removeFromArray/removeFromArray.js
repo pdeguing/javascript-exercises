86509f0a9a256f3fe03fbe6b83380671973b1896
Pierre-Quentin Deguingand <39034711+pdeguing@users.noreply.github.com>
2024-12-19 07:53:00 +0100

    solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...args) {
+  let i = 0;
+  for (x = 1; x < arguments.length; x++) {
+    console.log(inputArray + " " + arguments[x]);
+    i = 0;
+    while (i != -1) {
+      i = inputArray.indexOf(arguments[x]);
+      if (i != -1) {
+        inputArray.splice(i, 1);
+      }
+    }
+    console.log(inputArray);
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
