b690bd39d084b8544f4bf8b56058841e7cd66d80
Pierre-Quentin Deguingand <39034711+pdeguing@users.noreply.github.com>
2024-12-19 08:05:50 +0100

    solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (begin, end) {
+  let sum = 0;
 
+  if (
+    begin < 0 ||
+    end < 0 ||
+    !Number.isInteger(begin) ||
+    !Number.isInteger(end)
+  )
+    return "ERROR";
+
+  if (begin > end) {
+    sum = end;
+    end = begin;
+    begin = sum;
+    sum = 0;
+  }
+  for (; begin <= end; begin++) {
+    sum += begin;
+  }
+  return sum;
 };
 
 // Do not edit below this line
